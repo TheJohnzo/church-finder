454e76dc0a93247546e9cf3e442dc465b2e4a38e
TheJohnzo <johnzo1@gmail.com>
2020-01-19 13:16:32 -0500

    upgrades and adding base sample data

---
+++ cleanup.sh
@@ -0,0 +1,4 @@
+php artisan cache:clear
+php artisan view:clear
+# we use custom code so we shouldn't do this except during upgrades
+# php artisan vendor:publish --provider="Cornford\Googlmapper\MapperServiceProvider" --tag=googlmapper
---
+++ sample_data_import/languages_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `language` (`id`, `code`, `primary_country`, `created_at`, `updated_at`)
+VALUES
+(1,'ja','jp',NULL,NULL),
+(2,'en','us',NULL,NULL);
