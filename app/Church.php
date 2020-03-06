9a6dfd22f6ee3c925ca49714edf7256eedc6edad
TheJohnzo <johnzo1@gmail.com>
2020-03-06 13:08:29 -0500

    Adjusting for memory problems on godaddy hosting

--- app/Church.php
+++ app/Church.php
@@ -61,16 +61,27 @@ class Church extends Model
      */
     public static function allChurchesNearLatLonRaw($latitude, $longitude, $distance = 20)
     {
+        $temp_table_name = str_replace(['-', '.', ' '], '_', "temp_" . $latitude . '_' . $longitude . '_' . $distance . '_' . microtime());
+        $temp_q = "
+            CREATE TEMPORARY TABLE $temp_table_name 
+            SELECT c.id church_id, c.*,(((acos(sin(( $latitude *pi()/180)) * sin((latitude*pi()/180))+cos(( $latitude *pi()/180)) * 
+                    cos((latitude*pi()/180)) * cos((( $longitude - longitude)*pi()/180))))*180/pi())*60*1.1515*1.609344) as distance
+            FROM church c 
+            JOIN church_address a ON (c.id = a.church_id)
+        ";
+        DB::select($temp_q);
+
         //TODO how will this handle multiple addresses???
         $q = "
-        SELECT church_id, distance FROM (
-            SELECT c.*,(((acos(sin(( :lat *pi()/180)) * sin((`latitude`*pi()/180))+cos(( :lat2 *pi()/180)) * 
-                    cos((`latitude`*pi()/180)) * cos((( :lon - `longitude`)*pi()/180))))*180/pi())*60*1.1515*1.609344) as distance
-            FROM church_address_view c 
-        ) foo
+        SELECT church_id, distance FROM $temp_table_name
         WHERE distance <= :distance
         ORDER BY distance";
-        return DB::select($q, ['lat' => $latitude, 'lat2' => $latitude, 'lon' => $longitude, 'distance' => $distance]);
+        $data = DB::select($q, ['distance' => $distance]);
+
+        $drop_q = "DROP TEMPORARY TABLE $temp_table_name";
+        DB::select($drop_q);
+
+        return $data;
     }
 
     /**
